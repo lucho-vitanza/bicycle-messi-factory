54f90b5a93453603b04ed2d51f435b87739018b0
NataN <vasquezvalle@hotmail.com>
2023-05-29 22:28:53 -0500

    Feat: Adding new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,120 +1,66 @@
 import React, { useState } from "react";
-import { useRoutes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Web3 from "web3";
 import { ContractABI } from "./abi/ContractABI";
-import Login from '../src/components/Login';
-import Warehouse from '../src/components/Warehouse';
-import FactoryCard from '../src/components/FactoryCard';
-import { dbBicycle } from '../src/db/dbBicycle';
-import Home from '../src/Pages/Home';
-import MyAccount from '../src/Pages/MyAccount';
-import MyOrder from '../src/Pages/MyOrder';
-import MyOroders from '../src/Pages/MyOrders';
-import SignIn from '../src/Pages/SignIn';
-import Navbar from '../src/components/Navbar';
-import NotFound from '../src/Pages/Notfound';
-import './App.css'
+import Login from "../src/components/Login";
+import Warehouse from "../src/components/Warehouse";
+import FactoryCard from "../src/components/FactoryCard";
+import { dbBicycle } from "../src/db/dbBicycle";
+import Home from "../src/Pages/Home";
+import MyAccount from "../src/Pages/MyAccount";
+import MyOrder from "../src/Pages/MyOrder";
+import MyOrders from "../src/Pages/MyOrders";
+import SignIn from "../src/Pages/SignIn";
+import Navbar from "../src/components/Navbar";
+import NotFound from "../src/Pages/Notfound";
+import "./App.css";
 
+const web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+web3.eth.defaultAccount = web3.eth.accounts[0];
 
-  const web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
-  web3.eth.defaultAccount = web3.eth.accounts[0];
-
-  const RemixContract = new web3.eth.Contract(
+const RemixContract = new web3.eth.Contract(
   ContractABI,
   "0x9Ab60476c94a0E2f98A6E741b9E726AEBd6B4c5f"
 );
 
-
-
-
 function App() {
-  
   const [message, setMessage] = useState("");
 
+  const setData = async (e) => {
+    e.preventDefault();
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
 
-  const setData = async e => {
-     e.preventDefault();
-      const accounts = await window.ethereum.enable();
-      const account = accounts[0];
-
-  
-  const gas = await RemixContract.methods.setMessage(message).estimateGas();
-  const result = await RemixContract.methods
-       .setMessage(message)
-       .send({ from: account, gas });
+    const gas = await RemixContract.methods.setMessage(message).estimateGas();
+    const result = await RemixContract.methods
+      .setMessage(message)
+      .send({ from: account, gas });
     console.log(result);
   };
-  
-  const getDefaultData = async e => {
-     RemixContract.methods
-      .defaultMessage()
-       .call()
-      .then(console.log);
+
+  const getDefaultData = async (e) => {
+    RemixContract.methods.defaultMessage().call().then(console.log);
   };
 
+  const getData = async (e) => {
+    RemixContract.methods.getMessage().call().then(console.log);
+  };
 
-  const getData = async e => {
-    RemixContract.methods
-      .getMessage()
-      .call()
-      .then(console.log);
-   };
-  
- console.log("db en app", dbBicycle)
+  console.log("db en app", dbBicycle);
 
   return (
-    <div className='App'>
-        {/* <header className="App-header">
-          
-
-        {/* <form onSubmit={setData}>
-           <label>
-             Set Message:
-             <input
-              type="text"
-              name="message" 
-              value={message} 
-              onChange={e => setMessage(e.target.value)} 
-            />
-          </label>
-          <input type="submit" value="Set Message" />
-         </form> */}
-        <br />
-        {/* <button onClick={getData} type="button">
-          Get Message
-        </button>
-       <button onClick={getDefaultData} type="button">
-          Get Default Message
-        </button> */}
-       {/* </header> */} 
-    
-      <BrowserRouter>
-        <AppRoutes />
-        <Navbar />
-      </BrowserRouter> 
-      <Login />
-      <FactoryCard arrBicycle={dbBicycle} />
-      <Warehouse /> 
-    </div>
-    )
-  }
-
- const AppRoutes = () => {
-  let routes = useRoutes([
-    { path: '/Home', Element: <Home /> },
-    { path: '/src/Pages/my-account', Element: <MyAccount />},
-    { path: '/src/Pages/MyOrder', Element: <MyOrder /> },
-    { path: '/src/Pages/my-orders', Element: <MyOroders /> },
-    { path: '/src/Pages/sign-in', Element: <SignIn /> },
-    { path: '/*', Element: <NotFound />},
-    
-   ])
-
-   return routes
-};
-
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/MyOrder" element={<MyOrder />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+        <Route path="my-account" element={<MyAccount />} />
+      </Routes>
+    </Router>
+  );
+}
 
 
 
 export default App;
-
--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,119 +1,40 @@
+import { NavLink } from "react-router-dom";
 
-import { NavLink} from 'react-router-dom'
+const Navbar = () => {
+  const activeStyle = "underline underline-offset-4";
 
-
-
-
-
-const Navbar = () =>{
-    const activeStyle = 'underline underline-offset-4'
-
-    return(
-        <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
-            <ul className='flex items-center gap-3'>
-                <li className='font-semibold text-lg'>
-                    <NavLink to='/'>
-                        Shopping
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/montain' 
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                        Montain
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/route' 
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                        Route
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/electrical'  
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                        Electrical
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/urban'  
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                        Urban
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/fitness'  
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                        Fitness
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/others'  
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                        Others
-                    </NavLink>
-                </li>
-            </ul>
-             <ul className='flex items-center gap-3'>
-                <li className='text-black/60'>
-                   kellysalb56@gmail.com
-                </li>
-                <li>
-                    <NavLink 
-                        to='/my-account' 
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                        My Account
-                    </NavLink>
-                </li>
-                 <li>
-                    <NavLink 
-                        to='/my-order' 
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                      My Order
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/my-orders' 
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                      My Orders
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/sign-in'  
-                        className={({ isActive}) =>
-                        isActive ? activeStyle : undefined
-                        }>
-                        Sign In
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
+      <ul className="flex items-center gap-3">
+        <li>
+          <NavLink
+            to="/Home"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Home
+          </NavLink>
+        </li>
+      </ul>
+      <ul className="flex items-center gap-3">
+        <li>
+          <NavLink
+            to="/my-orders"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            My Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/my-account"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            My Account
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar
